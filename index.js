b49a38b240e8e5eb5fe6c533372433428731cf78
Leyo98765 <124131665+Leyo98765@users.noreply.github.com>
2023-02-13 17:47:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
+// Import des bibliothèques nécessaires
+const Gpio = require('onoff').Gpio;
 const fs = require('fs');
-const re = require('socket.io-client');
-var frequence = 103.1;
 
-// Read the JSON file
-fs.readFile('./radios.json', 'utf-8', (err, data) => {
-    if (err) throw err;
+// Import des radios à partir du fichier JSON
+let rawdata = fs.readFileSync('radios.json');
+let radios = JSON.parse(rawdata).radios;
 
-    // Parse the JSON data
-    const radioData = JSON.parse(data);
+// Initialisation de la variable pour stocker l'index de la radio actuelle
+let currentRadioIndex = 0;
 
-    // Get the radio frequency passed as parameter
-    //const radioFrequency = process.argv[2];
+// Initialisation des ports GPIO pour les signaux de l'encodeur rotatif
+const gpioRotaryA = new Gpio(38, 'in', 'both');
+const gpioRotaryB = new Gpio(36, 'in', 'both');
+const gpioRotarySw = new Gpio(34, 'in', 'both');
 
-    // Search for the radio with the given frequency
-    let freqFM = frequence.toString() + " FM";
-    const radio = radioData.radios.find(radio => radio.frequency === freqFM);
-    console.log(radio);
+// Fonction pour jouer la radio précédente
+function playPreviousRadio() {
+  currentRadioIndex--;
+  if (currentRadioIndex < 0) {
+    currentRadioIndex = radios.length - 1;
+  }
+  playRadio(radios[currentRadioIndex].url);
+}
 
-    // Check if the radio was found
-    if (radio) {
-        // Get the URL of the radio
-            const radioURL = radio.url;
-            const radioURlString = radioURL.toString();
-            console.log(radioURL);
-            // Play the radio using Volumio
-            const socket = re.io("http://volumio.local");
-            socket.emit('play', {"value": radioURlString});
-            console.log("Playing webRadio : " + radioURlString);
-            //socket.close();
-    } else {
-        console.error(`Radio with frequency ${freqFM} not found`);
-    }
+// Fonction pour mettre en pause ou jouer la radio actuelle
+function playPauseRadio() {
+  // Mettre en pause ou jouer la radio en utilisant l'API Volumio
+  // ...
+}
+
+// Ecouter les signaux de l'encodeur rotatif
+gpioRotaryA.watch((err, value) => {
+  // Si le signal A change de valeur
+  if (value !== 0) {
+    // Lire la radio suivante
+    playPreviousRadio();
+  }
 });
+
+gpioRotaryB.watch((err, value) => {
+  // Si le signal B change de valeur
+  if (value !== 0) {
+    // Lire la radio suivante
+    playNextRadio();
+  }
+});
+
+gpioRotarySw.watch((err, value) => {
+  // Si le signal de l'interrupteur change de valeur
+  if (value === 0) {
+    // Mettre en pause ou jouer la radio actuelle
+    playPauseRadio();
+  }
+});
+
+// Jouer la première radio à l'ouverture du plugin
+playRadio(radios[0].url);
